b30cea8f858e1b25fc0e6d42ebc3a36c2beec8d3
DESKTOP-4V48SB7\Equipo <esteban.smb.3008@gmail.com>
2017-01-21 15:31:39 -0400

    Listo

---
+++ Capitulo8Ejercicio2/Capitulo8Ejercicio2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Capitulo8Ejercicio2
+{
+    class Program
+    {
+        //Hacer un programa que le pida al usuario una cadena y la forma de justificarla.
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite la cadena a justificar: ");
+            string cad = Console.ReadLine();
+            Console.Write("Digite la cantidad de espacios para justificar: ");
+            int spaces = Convert.ToInt32(Console.ReadLine());
+            cad = cad.PadLeft(spaces);
+            Console.WriteLine("\nNueva cadena:\n" + cad);
+            Console.ReadKey();
+        }
+    }
+}
